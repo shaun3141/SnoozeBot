844ae0a3f41e8d01be2005e9aeb189c0a50c9498
shaun3141 <shaun.t.vanweelden@gmail.com>
2019-06-01 20:35:38 -0700

    Update snooze.js

--- models/snooze.js
+++ models/snooze.js
@@ -3,7 +3,7 @@ const db = require('./db.js');
 exports.snoozeConversation = function(conversationId, mailboxId, userId, openInSeconds, res) {
   // Calculate UTC Datetime that is now + openInSeconds
   let date = new Date();
-  let snoozeDate = date.setSeconds(date.getSeconds() + openInSeconds).toISOString();
+  let snoozeDate = new Date(date.setSeconds(date.getSeconds() + openInSeconds)).toISOString()
 
   // Create Snooze Object
   let snooze = {
