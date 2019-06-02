73033be99a2c5221a1e22c6106e5097e22942b79
shaun3141 <shaun.t.vanweelden@gmail.com>
2019-06-01 20:23:13 -0700

    Update snooze.js

--- models/snooze.js
+++ models/snooze.js
@@ -1,5 +1,26 @@
-exports.snoozeConversation = async function(conversationId, mailboxId, userId, openInSeconds, res) {
-  // Validate Credentials
+const db = require('./db.js');
 
+exports.snoozeConversation = function(conversationId, mailboxId, userId, openInSeconds, res) {
+  // Calculate UTC Datetime that is now + openInSeconds
+  let date = new Date();
+  let snoozeDate = date.setSeconds(date.getSeconds() + openInSeconds).toISOString();
+
+  // Create Snooze Object
+  let snooze = {
+    "id": conversationId,
+    "mailbox_id": mailboxId,
+    "user_id": userId,
+    "snooze_date": snoozeDate
+  }
+  
   // Add to DB
+  try {
+    db.updateOrCreate("snooze", snooze, function() {
+      res.send("Snooze is added!");
+    }, function(err) {
+      res.send("Error adding snooze | " + err);
+    });
+  } catch (e) {
+    res.send("Error adding snooze");
+  }
 }
