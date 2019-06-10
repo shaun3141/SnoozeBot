4f3577f575b74f2fc8f0ca198742def5ced342d1
shaun3141 <shaun.t.vanweelden@gmail.com>
2019-06-09 21:35:35 -0700

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,5 +1,12 @@
-var alarmClock = function(){
+// Go pure vanilla JS
+// https://www.freecodecamp.org/news/here-is-the-most-popular-ways-to-make-an-http-request-in-javascript-954ce8c95aaa/
+
+const alarmClock = function() {
   console.log("Waking up Snoozes");
-  fetch(new Request('https://hs-snoozebot.herokuapp.com/alarm_clock'));
+  const Http = new XMLHttpRequest();
+  const url='https://hs-snoozebot.herokuapp.com/alarm_clock';
+  Http.open("GET", url);
+  Http.send();
 }
+
 setInterval(alarmClock, 15000);
