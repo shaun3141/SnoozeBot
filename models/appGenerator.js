7679a39e21883f0e94a100d0d6f37f14cc137446
shaun3141 <shaun.t.vanweelden@gmail.com>
2019-06-12 02:39:47 -0700

    Support Custom Domain

--- models/appGenerator.js
+++ models/appGenerator.js
@@ -1,7 +1,7 @@
 const db = require('./db.js');
 const auth = require('./auth.js');
 
-const BASE_URL = 'https://hs-snoozebot.herokuapp.com/'
+const BASE_URL = 'https://snooze-bot.com/'
 
 exports.getApp = async function(metadata, res) {
   const mailboxId = metadata.mailbox.id;
